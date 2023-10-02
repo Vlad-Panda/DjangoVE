f3bb48bc4bcd445a2715d7c29c5bc9d85b074029
Vlad-Panda <pandarov.vladyslav@chnu.edu.ua>
2023-10-02 21:26:34 +0300

    #9: lab3

--- mysite/app_blog/views.py
+++ mysite/app_blog/views.py
@@ -5,3 +5,11 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
  def get(self, request, **kwargs):
      return render(request, 'index.html', context=None)
+
+from django.views.generic import DetailView
+from .models import Article
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'  # Шлях до шаблону
+    context_object_name = 'article'  # Ім'я змінної в контексті шаблону
